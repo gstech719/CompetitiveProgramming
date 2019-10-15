3fb76a11643f47f7271ae69c56dc49d042f0b1ca
gstech719 <gs.tech.geek719@gmail.com>
2019-10-16 02:32:31 +0530

    added CountCharacterOccurance

---
+++ CharacterOccurance/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterOccurance
+{
+    class Program
+    {
+
+        static string CountCharacterOccurance(string input, char charToFind)
+        {
+            string result = string.Empty;
+            int count = 0;
+            foreach (char ch in input)
+            {
+                if (ch == charToFind)
+                {
+                    count++;
+                }
+            }
+            result = $"{charToFind} is {count}";
+            return result;
+        }
+        static string CountCharacterOccurance(string input)
+        {
+            string result = string.Empty;
+            char charToFind;
+            string[] strArray = new string[input.Length];
+            int count = 1;
+            for (int i = 0; i < input.Length; i++)
+            {
+                charToFind = input[i];
+                for (int j = 1; j < input.Length; j++)
+                {
+                    if (input[i] == input[j])
+                    {
+                        count++;
+                    }
+                }
+                strArray[i] = $"{charToFind}{count}";
+                count = 1;
+            }
+            Array.Sort(strArray);
+            result = string.Join(string.Empty, strArray);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Input string");
+                var input = Console.ReadLine();
+                // Console.WriteLine("Input character to find");
+                //var charToFind = Console.ReadKey().KeyChar;
+                //var result = CountCharacterOccurance(input, charToFind);
+                var result1 = CountCharacterOccurance(input);
+                //Console.WriteLine(result);
+                Console.WriteLine(result1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
